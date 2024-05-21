2673a8a549a011d21d5903f23f8baf262122d9e3
vkjdinesh <vkjdinesh@gmail.com>
2024-05-21 23:43:04 +0300

    changed the code

---
+++ app.py
@@ -0,0 +1,446 @@
+import os
+import requests
+import spacy
+from spacy.lang.en import English
+import random
+import re
+from sentence_transformers import SentenceTransformer
+import torch
+import numpy as np
+import pandas as pd
+import textwrap
+import matplotlib.pyplot as plt
+from transformers.models.bert.configuration_bert import BertConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import BitsAndBytesConfig
+import time
+from transformers.utils import is_flash_attn_2_available
+from sentence_transformers import util, SentenceTransformer
+
+pdf_path = 'Nutrition.pdf'
+if not os.path.exists(pdf_path):
+  print('file does not exist, pdf downloading')
+  url = 'https://pressbooks.oer.hawaii.edu/humannutrition2/open/download?type=pdf'
+  filename=pdf_path
+  response = requests.get(url)
+  if response.status_code == 200:
+    with open(filename, 'wb') as file:
+      file.write(response.content)
+      print(f'file has been downloaded and saved as {filename}')
+  else:
+    print(f'file has not been downloaded and the status code is {response.status_code}')
+else:
+  print(f'file {pdf_path} exist')
+
+from tqdm.auto import tqdm
+import fitz
+def text_formatter(text: str) -> str:
+    cleaned_text = text.replace("\n", " ").strip()
+    return cleaned_text
+def open_and_read_pdf(pdf_path: str) -> list[dict]:
+    doc = fitz.open(pdf_path)
+    pages_and_texts = []
+    for page_number, page in tqdm(enumerate(doc)):
+      text = page.get_text()
+      #print(type(text))
+      text = text_formatter(text)
+      pages_and_texts.append({
+        "page number": page_number,
+        "page_char_count": len(text),
+        "page_word_count": len(text.split(" ")),
+        "page_sentence_count_raw": len(text.split(". ")),
+        "page_token_count": len(text) / 4,  # 1 token = ~4 chars, see: https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them
+        "text": text})
+    return pages_and_texts
+pages_and_texts = open_and_read_pdf(pdf_path=pdf_path)
+
+df=pd.DataFrame(pages_and_texts)
+df.describe().round(2)
+column_data_subset = df['text']
+nlp = English()
+nlp.add_pipe('sentencizer')
+for item in tqdm(pages_and_texts):
+  item['sentences']=list(nlp(item['text']).sents)
+  item["sentences"] = [str(sentence) for sentence in item["sentences"]]
+  # Count the sentences
+  item["page_sentence_count_spacy"] = len(item["sentences"])
+
+df = pd.DataFrame(pages_and_texts)
+num_sentence_chunk_size = 10
+def split_list(input_list:list,
+               slice_size:int)-> list[list[str]]:
+    return [input_list[i:i + slice_size] for i in range(0, len(input_list), slice_size)]
+
+for item in tqdm(pages_and_texts):
+    item["sentence_chunks"] = split_list(input_list=item["sentences"],
+                                         slice_size=num_sentence_chunk_size)
+    item["num_chunks"] = len(item["sentence_chunks"])
+
+df = pd.DataFrame(pages_and_texts)
+df.describe().round(2)
+
+df.describe().round(2)
+pages_and_chunks = []
+for item in tqdm(pages_and_texts):
+    for sentence_chunk in item["sentence_chunks"]:
+        chunk_dict = {}
+        chunk_dict["page_number"] = item["page number"]
+
+        # Join the sentences together into a paragraph-like structure, aka a chunk (so they are a single string)
+        joined_sentence_chunk = "".join(sentence_chunk).replace("  ", " ").strip()
+        joined_sentence_chunk = re.sub(r'\.([A-Z])', r'. \1', joined_sentence_chunk) # ".A" -> ". A" for any full-stop/capital letter combo
+        chunk_dict["sentence_chunk"] = joined_sentence_chunk
+
+        # Get stats about the chunk
+        chunk_dict["chunk_char_count"] = len(joined_sentence_chunk)
+        chunk_dict["chunk_word_count"] = len([word for word in joined_sentence_chunk.split(" ")])
+        chunk_dict["chunk_token_count"] = len(joined_sentence_chunk) / 4 # 1 token = ~4 characters
+
+        pages_and_chunks.append(chunk_dict)
+
+# How many chunks do we have?
+len(pages_and_chunks)
+
+df = pd.DataFrame(pages_and_chunks)
+df.describe().round(2)
+
+# Show random chunks with under 30 tokens in length
+min_token_length = 30
+for row in df[df["chunk_token_count"] <= min_token_length].sample(5).iterrows():
+    print(f'Chunk token count: {row[1]["chunk_token_count"]} | Text: {row[1]["sentence_chunk"]}')
+
+pages_and_chunks_over_min_token_len = df[df["chunk_token_count"] > min_token_length].to_dict(orient="records")
+# Get stats about our chunks
+df = pd.DataFrame(pages_and_chunks_over_min_token_len )
+df.describe().round(2)
+
+embedding_model = SentenceTransformer(model_name_or_path="all-mpnet-base-v2",
+                                      device="cpu") # choose the device to load the model to (note: GPU will often be *much* faster than CPU)
+
+# Send the model to the GPU
+embedding_model.to("cpu")
+
+# Create embeddings one by one on the GPU
+for item in tqdm(pages_and_chunks_over_min_token_len):
+    item["embedding"] = embedding_model.encode(item["sentence_chunk"])
+
+# Turn text chunks into a single list
+text_chunks = [item["sentence_chunk"] for item in pages_and_chunks_over_min_token_len]
+
+# Embed all texts in batches
+text_chunk_embeddings = embedding_model.encode(text_chunks,
+                                               batch_size=32, # you can use different batch sizes here for speed/performance, I found 32 works well for this use case
+                                               convert_to_tensor=True) # optional to return embeddings as tensor instead of array
+# Save embeddings to file
+text_chunks_and_embeddings_df = pd.DataFrame(pages_and_chunks_over_min_token_len)
+embeddings_df_save_path = "text_chunks_and_embeddings_df.csv"
+text_chunks_and_embeddings_df.to_csv(embeddings_df_save_path, index=False)
+
+text_chunks_and_embedding_df_load = pd.read_csv(embeddings_df_save_path)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# Import texts and embedding df
+text_chunks_and_embedding_df = pd.read_csv("text_chunks_and_embeddings_df.csv")
+
+# Function to convert embedding string to numpy array
+def convert_embedding(x):
+    try:
+        return np.fromstring(x.strip("[]"), sep=" ")
+    except AttributeError:
+        return np.nan  # or any other handling for NaN values
+
+# Apply function to convert embedding column
+text_chunks_and_embedding_df["embedding"] = text_chunks_and_embedding_df["embedding"].apply(convert_embedding)
+
+# Drop rows with missing embeddings
+text_chunks_and_embedding_df.dropna(subset=["embedding"], inplace=True)
+
+# Convert texts and embedding df to list of dicts
+pages_and_chunks = text_chunks_and_embedding_df.to_dict(orient="records")
+
+# Convert embeddings to torch tensor and send to device
+embeddings = torch.tensor(np.array(text_chunks_and_embedding_df["embedding"].tolist()), dtype=torch.float32).to(device)
+embeddings_shape = embeddings.shape
+
+# 1. Define the query
+# Note: This could be anything. But since we're working with a nutrition textbook, we'll stick with nutrition-based queries.
+print('Enter the prompt : ')
+query = input()
+print(f"Query: {query}")
+
+# 2. Embed the query to the same numerical space as the text examples
+# Note: It's important to embed your query with the same model you embedded your examples with.
+query_embedding = embedding_model.encode(query, convert_to_tensor=True)
+
+# 3. Get similarity scores with the dot product (we'll time this for fun)
+from time import perf_counter as timer
+
+start_time = timer()
+dot_scores = util.dot_score(a=query_embedding, b=embeddings)[0]
+end_time = timer()
+
+print(f"Time take to get scores on {len(embeddings)} embeddings: {end_time-start_time:.5f} seconds.")
+
+# 4. Get the top-k results (we'll keep this to 5)
+top_results_dot_product = torch.topk(dot_scores, k=5)
+
+larger_embeddings = torch.randn(100*embeddings.shape[0], 768).to(device)
+print(f"Embeddings shape: {larger_embeddings.shape}")
+
+# Perform dot product across 168,000 embeddings
+start_time = timer()
+dot_scores = util.dot_score(a=query_embedding, b=larger_embeddings)[0]
+end_time = timer()
+
+print(f"Time take to get scores on {len(larger_embeddings)} embeddings: {end_time-start_time:.5f} seconds.")
+
+# Define helper function to print wrapped text
+import textwrap
+
+def print_wrapped(text, wrap_length=80):
+    wrapped_text = textwrap.fill(text, wrap_length)
+    print(wrapped_text)
+
+print(f"Query: '{query}'\n")
+print("Results:")
+# Loop through zipped together scores and indicies from torch.topk
+for score, idx in zip(top_results_dot_product[0], top_results_dot_product[1]):
+    print(f"Score: {score:.4f}")
+    # Print relevant sentence chunk (since the scores are in descending order, the most relevant chunk will be first)
+    print("Text:")
+    print_wrapped(pages_and_chunks[idx]["sentence_chunk"])
+    # Print the page number too so we can reference the textbook further (and check the results)
+    print(f"Page number: {pages_and_chunks[idx]['page_number']}")
+    print("\n")
+
+import torch
+
+def dot_product(vector1, vector2):
+    return torch.dot(vector1, vector2)
+
+def cosine_similarity(vector1, vector2):
+    dot_product = torch.dot(vector1, vector2)
+
+    # Get Euclidean/L2 norm of each vector (removes the magnitude, keeps direction)
+    norm_vector1 = torch.sqrt(torch.sum(vector1**2))
+    norm_vector2 = torch.sqrt(torch.sum(vector2**2))
+
+    return dot_product / (norm_vector1 * norm_vector2)
+
+def retrieve_relevant_resources(query: str,
+                                embeddings: torch.tensor,
+                                model: SentenceTransformer=embedding_model,
+                                n_resources_to_return: int=5,
+                                print_time: bool=True):
+    """
+    Embeds a query with model and returns top k scores and indices from embeddings.
+    """
+
+    # Embed the query
+    query_embedding = model.encode(query,
+                                   convert_to_tensor=True)
+
+    # Get dot product scores on embeddings
+    start_time = timer()
+    dot_scores = util.dot_score(query_embedding, embeddings)[0]
+    end_time = timer()
+
+    if print_time:
+        print(f"[INFO] Time taken to get scores on {len(embeddings)} embeddings: {end_time-start_time:.5f} seconds.")
+
+    scores, indices = torch.topk(input=dot_scores,
+                                 k=n_resources_to_return)
+
+    return scores, indices
+
+def print_top_results_and_scores(query: str,
+                                 embeddings: torch.tensor,
+                                 pages_and_chunks: list[dict]=pages_and_chunks,
+                                 n_resources_to_return: int=5):
+    """
+    Takes a query, retrieves most relevant resources and prints them out in descending order.
+
+    Note: Requires pages_and_chunks to be formatted in a specific way (see above for reference).
+    """
+
+    scores, indices = retrieve_relevant_resources(query=query,
+                                                  embeddings=embeddings,
+                                                  n_resources_to_return=n_resources_to_return)
+
+    print(f"Query: {query}\n")
+    print("Results:")
+    # Loop through zipped together scores and indicies
+    for score, index in zip(scores, indices):
+        print(f"Score: {score:.4f}")
+        # Print relevant sentence chunk (since the scores are in descending order, the most relevant chunk will be first)
+        print_wrapped(pages_and_chunks[index]["sentence_chunk"])
+        # Print the page number too so we can reference the textbook further and check the results
+        print(f"Page number: {pages_and_chunks[index]['page_number']}")
+        print("\n")
+
+# Get just the scores and indices of top related results
+scores, indices = retrieve_relevant_resources(query=query,
+                                              embeddings=embeddings)
+scores, indices
+
+print_top_results_and_scores(query=query,
+                             embeddings=embeddings)
+
+import psutil
+
+# Get CPU available memory
+cpu_memory_bytes = psutil.virtual_memory().total
+cpu_memory_gb = round(cpu_memory_bytes / (2**30))
+print(f"Available CPU memory: {cpu_memory_gb} GB")
+
+# Define the model and quantization configuration based on CPU memory
+if cpu_memory_gb < 8:
+    print(f"Your available CPU memory is {cpu_memory_gb}GB, which may not be sufficient to run large LLMs locally without quantization.")
+    use_quantization_config = True
+    model_id = "google/gemma-2b-it"
+elif cpu_memory_gb < 16:
+    print(f"CPU memory: {cpu_memory_gb} GB | Recommended model: Gemma 2B with quantization.")
+    use_quantization_config = True
+    model_id = "google/gemma-2b-it"
+elif cpu_memory_gb < 32:
+    print(f"CPU memory: {cpu_memory_gb} GB | Recommended model: Gemma 2B without quantization or Gemma 7B with quantization.")
+    use_quantization_config = False
+    model_id = "google/gemma-2b-it"
+else:
+    print(f"CPU memory: {cpu_memory_gb} GB | Recommend model: Gemma 7B without quantization.")
+    use_quantization_config = False
+    model_id = "google/gemma-7b-it"
+
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# Set the model ID
+model_id = "microsoft/Phi-3-mini-4k-instruct"
+print(f"[INFO] Using model_id: {model_id}")
+
+# Instantiate tokenizer
+tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path=model_id)
+
+# Instantiate the model with trust_remote_code=True
+llm_model = AutoModelForCausalLM.from_pretrained(
+    pretrained_model_name_or_path=model_id,
+    trust_remote_code=True,  # Allow custom code to be run
+    torch_dtype=torch.float16,
+    low_cpu_mem_usage=False
+)
+
+# Optionally, send the model to the CPU if needed
+llm_model.to("cpu")
+
+
+input_text = query
+print(f"user:\n{input_text}")
+
+# Create prompt template for instruction-tuned model
+dialogue_template = [
+    {"role": "user",
+     "content": input_text}
+]
+
+
+# Apply the chat template
+prompt = tokenizer.apply_chat_template(conversation=dialogue_template,
+                                       tokenize=False, # keep as raw text (not tokenized)
+                                       add_generation_prompt=True)
+print(f"\nPrompt (formatted):\n{prompt}")
+
+# Tokenize the input text (turn it into numbers) and send it to GPU
+input_ids = tokenizer(prompt, return_tensors="pt").to("cpu")
+print(f"Model input (tokenized):\n{input_ids}\n")
+
+# Generate outputs passed on the tokenized input
+# See generate docs: https://huggingface.co/docs/transformers/v4.38.2/en/main_classes/text_generation#transformers.GenerationConfig
+outputs = llm_model.generate(**input_ids,
+                             max_new_tokens=256) # define the maximum number of new tokens to create
+
+outputs_decoded = tokenizer.decode(outputs[0])
+print(f"Model output (decoded):\n{outputs_decoded}\n")
+
+def prompt_formatter(query: str,
+                     context_items: list[dict]) -> str:
+    """
+    Augments query with text-based context from context_items.
+    """
+    # Join context items into one dotted paragraph
+    context = "- " + "\n- ".join([item["sentence_chunk"] for item in context_items])
+
+    # Create a base prompt with examples to help the model
+    # Note: this is very customizable, I've chosen to use 3 examples of the answer style we'd like.
+    # We could also write this in a txt file and import it in if we wanted.
+    base_prompt = """Based on the following context items, please answer the query.
+Give yourself room to think by extracting relevant passages from the context before answering the query.
+Don't return the thinking, only return the answer.
+Make sure your answers are as explanatory as possible.
+Use the following examples as reference for the ideal answer style.
+\nExample 1:
+Query: What are the fat-soluble vitamins?
+Answer: The fat-soluble vitamins include Vitamin A, Vitamin D, Vitamin E, and Vitamin K. These vitamins are absorbed along with fats in the diet and can be stored in the body's fatty tissue and liver for later use. Vitamin A is important for vision, immune function, and skin health. Vitamin D plays a critical role in calcium absorption and bone health. Vitamin E acts as an antioxidant, protecting cells from damage. Vitamin K is essential for blood clotting and bone metabolism.
+\nExample 2:
+Query: What are the causes of type 2 diabetes?
+Answer: Type 2 diabetes is often associated with overnutrition, particularly the overconsumption of calories leading to obesity. Factors include a diet high in refined sugars and saturated fats, which can lead to insulin resistance, a condition where the body's cells do not respond effectively to insulin. Over time, the pancreas cannot produce enough insulin to manage blood sugar levels, resulting in type 2 diabetes. Additionally, excessive caloric intake without sufficient physical activity exacerbates the risk by promoting weight gain and fat accumulation, particularly around the abdomen, further contributing to insulin resistance.
+\nExample 3:
+Query: What is the importance of hydration for physical performance?
+Answer: Hydration is crucial for physical performance because water plays key roles in maintaining blood volume, regulating body temperature, and ensuring the transport of nutrients and oxygen to cells. Adequate hydration is essential for optimal muscle function, endurance, and recovery. Dehydration can lead to decreased performance, fatigue, and increased risk of heat-related illnesses, such as heat stroke. Drinking sufficient water before, during, and after exercise helps ensure peak physical performance and recovery.
+\nNow use the following context items to answer the user query:
+{context}
+\nRelevant passages: <extract relevant passages from the context here>
+User query: {query}
+Answer:"""
+
+    # Update base prompt with context items and query
+    base_prompt = base_prompt.format(context=context, query=query)
+
+    # Create prompt template for instruction-tuned model
+    dialogue_template = [
+        {"role": "user",
+        "content": base_prompt}
+    ]
+
+    # Apply the chat template
+    prompt = tokenizer.apply_chat_template(conversation=dialogue_template,
+                                          tokenize=False,
+                                          add_generation_prompt=True)
+    return prompt
+
+scores, indices = retrieve_relevant_resources(query=query,
+                                              embeddings=embeddings)
+
+# Create a list of context items
+context_items = [pages_and_chunks[i] for i in indices]
+
+# Format prompt with context items
+prompt = prompt_formatter(query=query,
+                          context_items=context_items)
+
+input_ids = tokenizer(prompt, return_tensors="pt").to("cpu")
+
+# Generate an output of tokens
+outputs = llm_model.generate(**input_ids,
+                             temperature=0.7, # lower temperature = more deterministic outputs, higher temperature = more creative outputs
+                             do_sample=True, # whether or not to use sampling, see https://huyenchip.com/2024/01/16/sampling.html for more
+                             max_new_tokens=256) # how many new tokens to generate from prompt
+
+# Turn the output tokens into text
+output_text = tokenizer.decode(outputs[0])
+
+print(f"Query: {query}")
+
+pattern = r'Answer:(.*?)(?=Example \d+:|Now use the following context items to answer the user query:|$)'
+
+# Search for the pattern in the output_text
+match = re.search(pattern, output_text, re.DOTALL)
+
+if match:
+    answer = match.group(1).strip()
+    sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', answer)  # Split by sentences
+    print(f"Query: {query}")
+    print("Assistant Answer:")
+    for sentence in sentences:
+        print(sentence)
+else:
+    print("No answer found in the output text.")
